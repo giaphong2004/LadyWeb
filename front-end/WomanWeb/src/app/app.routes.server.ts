b8ec08ce1a352f5efe8c8ed882d722db7400a370
giaphong2004 <2200010465@nttu.edu.vn>
2025-11-19 18:07:03 +0700

    Add prerender false to angular json

--- front-end/WomanWeb/src/app/app.routes.server.ts
+++ front-end/WomanWeb/src/app/app.routes.server.ts
@@ -2,7 +2,48 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'library',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'experts',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'tools/menstrual',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'tools/ovulation',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'tools/pregnancy',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'tools/pregnancy-test',
+    renderMode: RenderMode.Prerender
+  },
+  // Dynamic routes - Tắt prerender, dùng Server
+  {
+    path: 'posts/:slug',
+    renderMode: RenderMode.Server // Thay vì Prerender
+  },
+  {
+    path: 'expert/:id',
+    renderMode: RenderMode.Server // Thay vì Prerender
+  },
+  {
+    path: 'chat/:expertId',
+    renderMode: RenderMode.Server // Thay vì Prerender
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server
   }
 ];
